b9c34a10b657f064514ca72576f065fafeb28c2a
ChahirSaid <chahirsaid18@gmail.com>
2023-03-17 08:25:02 -0700

    attempt 100000

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,19 +3,18 @@
 /**
  * main - Entry point
  * Description: a C programme that prints the alphabet in lowercase
- * 		using the putchar function only.
- * 		a for loop to iterate through the lowercase letters
- * 		from 'a' to 'z' and print them one by one using putchar.
+ *		using the putchar function only.
+ *		a for loop to iterate through the lowercase letters
+ *		from 'a' to 'z' and print them one by one using putchar.
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i;
+	char c;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		 putchar(i);
-	}
+	for (c = 'a'; c <= 'z'; ++c)
+		 putchar(c);
+
 	putchar('\n');
 	return (0);
 }
