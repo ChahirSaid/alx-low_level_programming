d58e53219a035b9c596534179148aa7518c313a9
ChahirSaid <chahirsaid18@gmail.com>
2023-03-17 07:43:46 -0700

    i hate betty

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,12 +2,12 @@
 /**
  *main - Entry point
  *Description: declare and initialize the loop variable i to 'a', and iterate
- *		 through all the lowercase letters up to 'z' using a for loop.
- * 		check if the current letter is 'e' or 'q' using an if statement.
- * 		If it is, we use the continue keyword to skip the current
- * 		iteration and move on to the next letter.
- * 		Otherwise, print the current letter using putchar
- * 		folowed by a new line
+ *		through all the lowercase letters up to 'z' using a for loop.
+ *		check if the current letter is 'e' or 'q' using an if statement.
+ *		If it is, we use the continue keyword to skip the current
+ *		iteration and move on to the next letter.
+ *		Otherwise, print the current letter using putchar
+ *		folowed by a new line
  *
  *Return: Always 0 (Success)
  */
