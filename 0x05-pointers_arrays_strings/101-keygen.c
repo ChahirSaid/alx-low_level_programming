fef23c396f156e7edf0ed980e1a6105726e6cbb2
ChahirSaid <chahirsaid18@gmail.com>
2023-03-27 07:34:54 -0700

    mandatory + advanced tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ *	passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char pswd[84];
+	int i = 0;
+	int sum = 0;
+	int a, b;
+
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		pswd[i] = 33 + rand() % 94;
+		sum += pswd[i++];
+	}
+	pswd[i] = '\0';
+	if (sum != 2772)
+	{
+		a = (sum - 2772) / 2;
+		b = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			a++;
+		for (i = 0; pswd[i]; i++)
+		{
+			if (pswd[i] >= (33 + a))
+			{
+				pswd[i] -= a;
+				break;
+			}
+		}
+		for (i = 0; pswd[i]; i++)
+		{
+			if (pswd[i] >= (33 + b))
+			{
+				pswd[i] -= b;
+				break;
+			}
+		}
+	}
+	printf("%s", pswd);
+	return (0);
+}
