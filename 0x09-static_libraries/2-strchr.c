9af19a62323b8e0fdf5694897e5d2fc6c1c406e8
ChahirSaid <chahirsaid18@gmail.com>
2023-04-08 13:43:51 +0000

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string.
+ * @s: String
+ * @c: character
+ * Return: NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+
+	return (NULL);
+}
